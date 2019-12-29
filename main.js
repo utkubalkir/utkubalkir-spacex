f52ab81e5bd849c808cb57e4245f2af8b5b4690a
utkubalkir <33158193+utkubalkir@users.noreply.github.com>
2019-12-29 08:46:10 +0300

    first

---
+++ main.js
@@ -0,0 +1,200 @@
+
+        window.data;
+        window.rockets;
+        var work_year ;
+        var work_rocket ;
+
+        function getData(url)
+        {   //Retrieves the previous or next data from SpaceX
+            clcTimeOuts(); 
+            var ourRequest= new XMLHttpRequest()
+ 
+            ourRequest.open('GET',url,true);
+            ourRequest.onload =function()
+            { 
+             window.data= JSON.parse(ourRequest.responseText);
+             document.getElementById("flight_number").innerHTML="Flight Number : "+  window.data["flight_number"];
+             document.getElementById("mission_name").innerHTML="Misson Name : "+  window.data["mission_name"];
+             document.getElementById("rocket_name").innerHTML="Rocket Name : "+  window.data.rocket["rocket_name"];
+             document.getElementById("launch_site_name").innerHTML="Launch Site Name : "+  window.data.launch_site["site_name"];
+             
+             getCountdown(data["launch_date_unix"]);
+            }
+             ourRequest.send();
+        }
+        
+        function getCountdown(time)
+        {
+            //Shows the countdown timer or the time the rocket was launched.
+            if(time*1000 > Date.now())
+            {
+            var timestamp = (time*1000) - Date.now();
+            timestamp /= 1000; 
+
+            window.myTimer = setInterval(function()
+            {  
+            timestamp--; 
+
+            var days    = component(timestamp, 24 * 60 * 60),      
+            hours   = component(timestamp,      60 * 60) % 24, 
+            minutes = component(timestamp,           60) % 60, 
+            seconds = component(timestamp,            1) % 60; 
+            
+            document.getElementById("launch_state").innerHTML="Will be launched " +  days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds"
+                 
+            }, 1000);     
+            }
+
+            else
+            {
+            var launchedDate = new Date(time*1000)
+            clearInterval(window.myTimer);
+  
+            document.getElementById("launch_state").innerHTML = null;
+            document.getElementById("launch_state").innerHTML ="Launched "+ launchedDate;  
+            }
+
+        }
+
+        function getFlightNumber()
+        {   
+            //Gets the flight number in input.
+            var fnumber = document.getElementById("f_number").value ;
+            getByFlightNumber('https://api.spacexdata.com/v3/launches?flight_number='+fnumber);
+        }
+
+        function getByFlightNumber(url)
+        {
+            //Returns the data of the desired flight number.
+            clcTimeOuts(); 
+            var ourRequest= new XMLHttpRequest()
+ 
+            ourRequest.open('GET',url,true);
+            ourRequest.onload =function()
+            { 
+             window.data= JSON.parse(ourRequest.responseText);
+             document.getElementById("flight_number").innerHTML ="Flight Number : "+  window.data[0].flight_number;
+             document.getElementById("mission_name").innerHTML ="Mission Name : "+  window.data[0].mission_name;
+             document.getElementById("rocket_name").innerHTML ="Rocket Name : "+  window.data[0].rocket.rocket_name
+             document.getElementById("launch_site_name").innerHTML ="Launch Site Name : "+  window.data[0].launch_site.site_name
+             
+             getCountdown( window.data[0].launch_date_unix);
+            }
+    
+            ourRequest.send();
+        }
+
+        function getYear()
+        {
+            //Returns the year in input.
+            var year = document.getElementById("year").value ;
+            getByYear('https://api.spacexdata.com/v3/launches?launch_year='+year);
+        }
+
+        function getByYear(url)
+        {
+            //Returns data for all flights in that year.
+            clcTimeOuts();
+            var ourRequest= new XMLHttpRequest()
+   
+            ourRequest.open('GET',url,true);
+            ourRequest.onload =function ()
+            {
+             window.data= JSON.parse(ourRequest.responseText);  
+             for(var i =0 ;i< window.data.length;i++)
+             {        
+                work_year= setTimeout(writeByYear,i*5000,window.data,i);
+             }
+            }
+            ourRequest.send();
+
+        }
+
+        function writeByYear(data,i)
+        {
+            //Displays all flights of the year one by one every 5 seconds.
+            document.getElementById("flight_number").innerHTML ="Flight Number : "+ data[i].flight_number;
+            document.getElementById("mission_name").innerHTML ="Mission Name : "+ data[i].mission_name;
+            document.getElementById("rocket_name").innerHTML ="Rocket Name : "+ data[i].rocket.rocket_name
+            document.getElementById("launch_site_name").innerHTML ="Launch Site Name : "+ data[i].launch_site.site_name
+            getCountdown(data[i].launch_date_unix);
+        }
+
+        function getRockets()
+        {
+            //Gets all rocket names and adds them to the selection list.
+            var url = "https://api.spacexdata.com/v3/rockets";
+            var ourRequest= new XMLHttpRequest()
+
+            ourRequest.open('GET',url,true);
+            ourRequest.onload =function()
+            { 
+            window.rockets= JSON.parse(ourRequest.responseText);
+            for(var i =0; i<window.rockets.length;i++)
+            {
+                 var rocket = document.getElementById("rockets");
+                 var rocket_name = document.createElement("option");
+                 rocket_name.text = window.rockets[i].rocket_name;
+                 rocket.add(rocket_name); 
+            } 
+            }
+            ourRequest.send();
+
+        }
+
+        function getByRockets()
+        {
+            //Filters all flights according to the selected rocket name.
+            clcTimeOuts();
+            var select = document.getElementById("rockets");
+            var selected = select.options[select.selectedIndex].text;
+            var url = "https://api.spacexdata.com/v3/launches?rocket_name="+selected
+
+            var ourRequest= new XMLHttpRequest()
+
+            ourRequest.open('GET',url,true);
+            ourRequest.onload =function()
+            { 
+            window.data= JSON.parse(ourRequest.responseText);
+            for(var i =0 ;i<window.data.length;i++)
+            {        
+               work_rocket= setTimeout(writeByRocket,i*5000,window.data,i);
+            }      
+            }
+            ourRequest.send();
+        }
+
+        function writeByRocket(data,i)
+        {
+            //Displays data one by one according to the selected rocket name every 5 seconds.
+            document.getElementById("flight_number").innerHTML ="Flight Number : "+ data[i].flight_number;
+            document.getElementById("mission_name").innerHTML ="Mission Name : "+ data[i].mission_name;
+            document.getElementById("rocket_name").innerHTML ="Rocket Name : "+ data[i].rocket.rocket_name
+            document.getElementById("launch_site_name").innerHTML ="Launch Site Name : "+ data[i].launch_site.site_name
+            getCountdown(data[i].launch_date_unix);
+
+        }
+
+        function clcTimeOuts()
+        {
+            //stops all timeouts waiting to run.
+            while (work_year>=0)
+            {
+                
+                clearTimeout(work_year); 
+                work_year--;
+            }  
+            while(work_rocket>=0)
+            {
+                clearTimeout(work_rocket); 
+                work_rocket--;
+            }
+        }
+
+        function component(x, v)
+        {
+        return Math.floor(x / v);
+        }
+
+       
+   
